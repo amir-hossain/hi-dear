9a4622dc20cd73b627282b86622987af3571a841
amir <amir.hossain@bongobd.com>
2021-02-08 02:05:56 +0600

    splash design added

--- app/src/main/java/com/hi/dear/ui/activity/splash/SplashScreenActivity.kt
+++ app/src/main/java/com/hi/dear/ui/activity/splash/SplashScreenActivity.kt
@@ -3,10 +3,12 @@ package com.hi.dear.ui.activity.splash
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
 import com.hi.dear.databinding.ActivitySplashBinding
 import com.hi.dear.ui.PrefsManager
 import com.hi.dear.ui.activity.login.LoginActivity
 import com.hi.dear.ui.activity.main.MainActivity
+import com.hi.dear.ui.activity.register.RegistrationActivity
 import com.hi.dear.ui.base.BaseActivity
 
 class SplashScreenActivity : BaseActivity() {
@@ -15,12 +17,22 @@ class SplashScreenActivity : BaseActivity() {
         val binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val isLoggedIn = PrefsManager.getInstance(this).readBoolean(PrefsManager.IS_LOGGED_IN)
-        Handler(mainLooper).postDelayed({
-            if (isLoggedIn) {
+        if (isLoggedIn) {
+            Handler(mainLooper).postDelayed({
                 startActivity(Intent(applicationContext, MainActivity::class.java))
-            } else {
-                startActivity(Intent(applicationContext, LoginActivity::class.java))
-            }
-        }, 3000)
+                finish()
+            }, 2000)
+        } else {
+            binding.loginBtn.visibility = View.VISIBLE
+            binding.signUpBtn.visibility = View.VISIBLE
+        }
+
+        binding.loginBtn.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+        binding.signUpBtn.setOnClickListener {
+            startActivity(Intent(this, RegistrationActivity::class.java))
+        }
+
     }
 }
