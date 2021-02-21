26069727f9d5554f2c66f3ae121ca24acf76f604
amir <amir.hossain@bongobd.com>
2021-02-22 02:18:08 +0600

    tips page added

--- app/src/main/java/com/hi/dear/ui/fragment/tips/TipsFragment.kt
+++ app/src/main/java/com/hi/dear/ui/fragment/tips/TipsFragment.kt
@@ -1,35 +1,35 @@
 package com.hi.dear.ui.fragment.tips
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.hi.dear.R
 import com.hi.dear.databinding.FragmentTipsBinding
-import com.hi.dear.ui.DialogFactory
 import com.hi.dear.ui.base.BaseFragment
 
 
-class TipsFragment : BaseFragment(), DialogFactory.ITwoBtnListener {
+class TipsFragment : BaseFragment() {
+
+    private lateinit var binding: FragmentTipsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentTipsBinding.inflate(inflater)
-        DialogFactory.makeDialog(R.string.welcome, this)
-            .showDialog(activity?.supportFragmentManager)
-
+        binding = FragmentTipsBinding.inflate(inflater)
         return binding.root
     }
 
-    override fun onPositiveBtnClicked() {
-
-    }
-
-    override fun onNegativeBtnClicked() {
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.splashGroup.visibility = View.VISIBLE
+        binding.contentGroup.visibility = View.GONE
+        Handler(Looper.getMainLooper()).postDelayed({
+            binding.splashGroup.visibility = View.GONE
+            binding.contentGroup.visibility = View.VISIBLE
+        }, 2000)
     }
 
 }
