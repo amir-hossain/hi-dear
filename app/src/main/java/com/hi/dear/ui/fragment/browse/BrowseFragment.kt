3434f84c597f457a183052e5ec0f25b4255ae177
amir <amir.hossain@bongobd.com>
2021-04-30 12:13:03 +0600

    test ad added

--- app/src/main/java/com/hi/dear/ui/fragment/browse/BrowseFragment.kt
+++ app/src/main/java/com/hi/dear/ui/fragment/browse/BrowseFragment.kt
@@ -5,6 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.ads.*
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
+import com.hi.dear.R
 import com.hi.dear.data.model.common.UserCore
 import com.hi.dear.databinding.FragmentBrowseBinding
 import com.hi.dear.repo.BrowseRepository
@@ -14,12 +18,16 @@ import com.hi.dear.ui.Utils
 import com.hi.dear.ui.activity.ViewModelFactory
 import com.hi.dear.ui.base.BaseFragment
 import com.yuyakaido.android.cardstackview.*
+import timber.log.Timber
+
 
 class BrowseFragment : BaseFragment<FragmentBrowseBinding, BrowseViewModel>(), CardStackListener {
-
+    private val adCounterTarget = 5
+    private var swipeCounter = 0
     private var visibleUserData: UserCore? = null
     private val mAdapter by lazy { SwipeStackAdapter() }
     private val manager by lazy { CardStackLayoutManager(requireContext(), this) }
+    private var mInterstitialAd: InterstitialAd? = null
 
     override fun initViewBinding(inflater: LayoutInflater): FragmentBrowseBinding {
         return FragmentBrowseBinding.inflate(inflater)
@@ -32,6 +40,7 @@ class BrowseFragment : BaseFragment<FragmentBrowseBinding, BrowseViewModel>(), C
     }
 
     override fun initView() {
+        initAd()
         viewModel?.getBrowseData(getPreferredGender(), 5)
         manager.setStackFrom(StackFrom.BottomAndRight)
         manager.setSwipeableMethod(SwipeableMethod.Automatic)
@@ -48,6 +57,56 @@ class BrowseFragment : BaseFragment<FragmentBrowseBinding, BrowseViewModel>(), C
         binding.crossBtn.setOnClickListener {
             binding.swipeStack.swipe()
         }
+    }
+
+    private fun initAd() {
+        MobileAds.initialize(
+            requireContext()
+        ) {
+            val adRequest = AdRequest.Builder().build()
+            createintersitial(adRequest)
+        }
+    }
+
+    private fun createintersitial(adRequest: AdRequest) {
+        InterstitialAd.load(
+            requireContext(), getString(R.string.test_Interstitial_ad_id),
+            adRequest,
+            object : InterstitialAdLoadCallback() {
+                override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                    // The mInterstitialAd reference will be null until
+                    // an ad is loaded.
+                    mInterstitialAd = interstitialAd
+                    Timber.e("onAdLoaded")
+                    mInterstitialAd!!.fullScreenContentCallback =
+                        object : FullScreenContentCallback() {
+                            override fun onAdDismissedFullScreenContent() {
+                                // Called when fullscreen content is dismissed.
+                                Timber.d("The ad was dismissed.")
+                                initAd()
+                            }
+
+                            override fun onAdFailedToShowFullScreenContent(adError: AdError) {
+                                // Called when fullscreen content failed to show.
+                                Timber.d("The ad failed to show.")
+                            }
+
+                            override fun onAdShowedFullScreenContent() {
+                                // Called when fullscreen content is shown.
+                                // Make sure to set your reference to null so you don't
+                                // show it a second time.
+                                mInterstitialAd = null
+                                Timber.d("The ad was shown.")
+                            }
+                        }
+                }
+
+                override fun onAdFailedToLoad(loadAdError: LoadAdError) {
+                    // Handle the error
+                    Timber.e(loadAdError.message)
+                    mInterstitialAd = null
+                }
+            })
     }
 
     private fun getPreferredGender(): String {
@@ -121,12 +180,27 @@ class BrowseFragment : BaseFragment<FragmentBrowseBinding, BrowseViewModel>(), C
     }
 
     override fun onCardDisappeared(view: View?, position: Int) {
+        swipeCounter++
         if (position == mAdapter.itemCount - 1) {
             binding.contentGroup.visibility = View.GONE
             binding.emptyText.visibility = View.VISIBLE
+            showAd()
         } else {
             binding.contentGroup.visibility = View.VISIBLE
             binding.emptyText.visibility = View.GONE
+        }
+        if (swipeCounter >= adCounterTarget) {
+            showAd()
+        }
+    }
+
+    private fun showAd() {
+        Timber.d("showing ad")
+        if (mInterstitialAd != null) {
+            mInterstitialAd!!.show(requireActivity())
+            swipeCounter = 0
+        } else {
+            Timber.d("The interstitial ad wasn't ready yet.")
         }
     }
 }
