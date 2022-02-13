35d028c6ddb642724691304f83797eba0f7dcd21
Amir <mramir1971@gmail.com>
2022-02-14 02:27:39 +0600

    boost ui done

--- app/src/main/java/com/hi/dear/ui/fragment/boost/BoostProfileFragment.kt
+++ app/src/main/java/com/hi/dear/ui/fragment/boost/BoostProfileFragment.kt
@@ -2,12 +2,35 @@ package com.hi.dear.ui.fragment.boost
 
 import android.view.LayoutInflater
 import androidx.lifecycle.ViewModel
+import com.hi.dear.R
 import com.hi.dear.databinding.FragmentBoostProfileBinding
 import com.hi.dear.databinding.FragmentTipsBinding
+import com.hi.dear.ui.DialogFactory
+import com.hi.dear.ui.PrefsManager
+import com.hi.dear.ui.activity.login.LoginActivity
 import com.hi.dear.ui.base.BaseFragment
 
 
 class BoostProfileFragment : BaseFragment<FragmentBoostProfileBinding, ViewModel>() {
+    private val boost10BtnListener = object : DialogFactory.ITwoBtnListener {
+        override fun onPositiveBtnClicked() {
+
+        }
+
+        override fun onNegativeBtnClicked() {
+
+        }
+    }
+
+    private val boost30BtnListener = object : DialogFactory.ITwoBtnListener {
+        override fun onPositiveBtnClicked() {
+
+        }
+
+        override fun onNegativeBtnClicked() {
+
+        }
+    }
 
     override fun initViewBinding(inflater: LayoutInflater): FragmentBoostProfileBinding {
         return FragmentBoostProfileBinding.inflate(inflater)
@@ -18,7 +41,15 @@ class BoostProfileFragment : BaseFragment<FragmentBoostProfileBinding, ViewModel
     }
 
     override fun initView() {
+        binding.boost10.setOnClickListener {
+            DialogFactory.makeDialog(R.string.boost_10_confirm_msg, boost10BtnListener)
+                .showDialog(activity?.supportFragmentManager)
+        }
 
+        binding.boost30.setOnClickListener {
+            DialogFactory.makeDialog(R.string.boost_30_confirm_msg, boost30BtnListener)
+                .showDialog(activity?.supportFragmentManager)
+        }
     }
 
     override fun attachObserver(viewModel: ViewModel?) {
